6c08187ebb269626504d1084f90620c5a6480446
Scott Campit <scottcampit@gmail.com>
2023-08-01 23:17:55 -0700

    Add code to save frames to S3 bucket

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -10,5 +10,9 @@ fps=$(wget -qO - $url | ffprobe -i pipe:0 -v error -select_streams v -of default
 fps=${fps%/*}
 fps=$(bc <<< "scale=2; $fps")
 
+mkdir -p "./frames"
+
 # Extract 100 frames using the rounded fps value
-ffmpeg -i "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/5dhsSuUFnyR2lKOccs1r/pub/QxlMNmM32RCGSyCUjynU.mp4" -vf "fps=$fps" -vframes 100 "./frames/frame-%03d.jpg"
+ffmpeg -i "$url" -vf "fps=$fps" -vframes 100 "./frames/frame-%03d.jpg"
+
+aws s3 sync "./frames" "s3://your-bucket-name/frames"
